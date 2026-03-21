7d109843899d417894a6dcc88a81232cd547f7e3
Li Yuxuan <xmliszt@gmail.com>
2026-03-21 14:13:12 +0800

    feat(1.16): complete localization

--- Joodle/Views/MoveDrawingOverlayView.swift
+++ Joodle/Views/MoveDrawingOverlayView.swift
@@ -134,7 +134,7 @@ struct MoveDrawingBottomBar: View {
 
   var body: some View {
     HStack(spacing: 16) {
-      Text(String(localized: "Choose a date to move your doodle there"))
+      Text(String(localized: "Tap any date to move your doodle there"))
         .font(.appSubheadline())
         .foregroundColor(.textColor)
         .lineLimit(1)
